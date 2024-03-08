c9bc7be23ac47d0cedcec34aad01bd597d4dd0ba
Ricard Wanderlof <ricard2013@butoba.net>
2024-03-09 00:56:27 +0100

    MiMi-d: Add top level DPF API and Makefiles

---
+++ plugins/mimid/DistrhoPluginInfo.h
@@ -0,0 +1,38 @@
+/*
+	==============================================================================
+	This file is part of the MiMi-d synthesizer.
+
+	Copyright 2024 Ricard Wanderlof
+
+	This file may be licensed under the terms of of the
+	GNU General Public License Version 2 (the ``GPL'').
+
+	Software distributed under the License is distributed
+	on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+	express or implied. See the GPL for the specific language
+	governing rights and limitations.
+
+	You should have received a copy of the GPL along with this
+	program. If not, go to http://www.gnu.org/licenses/gpl.html
+	or write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+	==============================================================================
+ */
+
+#ifndef _DISTRHO_PLUGIN_INFO_H_
+#define _DISTRHO_PLUGIN_INFO_H_
+
+#define DISTRHO_PLUGIN_BRAND "Pollux"
+#define DISTRHO_PLUGIN_NAME  "MiMi-d"
+#define DISTRHO_PLUGIN_URI   "https://butoba.net/homepage/mimid.html"
+
+#define DISTRHO_PLUGIN_NUM_INPUTS   0
+#define DISTRHO_PLUGIN_NUM_OUTPUTS  2
+#define DISTRHO_PLUGIN_IS_SYNTH 1
+#define DISTRHO_PLUGIN_HAS_UI 0
+#define DISTRHO_PLUGIN_IS_RT_SAFE   1
+
+// Bring in Parameters enum */
+#include "Engine/ParamsEnum.h"
+
+#endif /* _DISTRHO_PLUGIN_INFO_H_ */
