43f3e13c712ee3151b847b151cfcc78bb02c9a5b
Ricard Wanderlof <ricard2013@butoba.net>
2024-03-12 15:50:41 +0100

    MiMi-d,ParamDefs: Add scale points
    Add scale points for enumerations and integer ranges (such as
    voice count, osc pitch and octave settings).

--- plugins/mimid/Engine/ParamsEnum.h
+++ plugins/mimid/Engine/ParamsEnum.h
@@ -28,6 +28,8 @@
 enum Parameters
 {
 
+#define PARAMPOINTS(SPID, ...)
+#define PARAMRANGE(SPID, MIN, MAX)
 #define PARAMGROUP(PGID, NAME, SYMBOL)
 #define PARAM(PARAMNO, PG, SP, NAME, SYMBOL, MIN, MAX, DEFAULT, SETFUNC) PARAMNO,
 #define PARAM_NULL(PARAMNO, NAME, SYMBOL) PARAMNO,
@@ -40,6 +42,8 @@ enum Parameters
 
 enum ParameterGroups
 {
+#define PARAMPOINTS(SPID, ...)
+#define PARAMRANGE(SPID, MIN, MAX)
 #define PARAMGROUP(PGID, NAME, SYMBOL) PGID,
 #define PARAM(PARAMNO, PG, SP, NAME, SYMBOL, MIN, MAX, DEFAULT, SETFUNC)
 #define PARAM_NULL(PARAMNO, NAME, SYMBOL)
@@ -47,4 +51,18 @@ enum ParameterGroups
 // This brings in the parameter groups as enum members
 #include "ParamDefs.h"
 
+};
+
+enum ScalePoints
+{
+#define PARAMPOINTS(SPID, ...) SPID,
+#define PARAMRANGE(SPID, MIN, MAX) SPID,
+#define PARAMGROUP(PGID, NAME, SYMBOL)
+#define PARAM(PARAMNO, PG, SP, NAME, SYMBOL, MIN, MAX, DEFAULT, SETFUNC)
+#define PARAM_NULL(PARAMNO, NAME, SYMBOL)
+
+// This brings in the scale points as enum members
+#include "ParamDefs.h"
+
+	SP_COUNT,
 };
