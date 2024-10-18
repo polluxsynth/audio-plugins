0a12193ad82c44ab196345b137fb58f0122b1064
Ricard Wanderlof <ricard2013@butoba.net>
2024-10-18 23:40:08 +0200

    Utils: find-missing-presets.sh: fix bug
    Fix bug causing patch names that were substrings of existing
    patches (such as Synth_Bass vs Synth_Bass_Long) to be missed
    during the search.

--- plugins/mimid/Utils/find-missing-presets.sh
+++ plugins/mimid/Utils/find-missing-presets.sh
@@ -4,5 +4,5 @@
 # Run from Presets directory
 manifest=manifest.ttl
 for i in [A-Z]*ttl; do
-  grep $i $manifest > /dev/null || echo "$i not found in $manifest"
+  grep "<$i>" $manifest > /dev/null || echo "$i not found in $manifest"
 done
