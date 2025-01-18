173cb38dab5e5d08036273ff2beec1c88ae1e115
Ricard Wanderlof <ricard2013@butoba.net>
2025-01-19 00:32:35 +0100

    SawOsc, PulseOsc, TriangleOsc: Precalculate n - 1
    Precalculate n - 1 (as nmask) at build time rather than (probably)
    having it happen at runtime.
    
    If nothing else, the resulting object file becomes smaller at least.

--- plugins/mimid/Engine/PulseOsc.h
+++ plugins/mimid/Engine/PulseOsc.h
@@ -29,11 +29,11 @@ class PulseOsc
 {
 	bool pw1t;
 	float buffer1[Samples*2];
-	const int n;
+	const int n, nmask;
 	float const *blepPTR;
 	int bP1;
 public:
-	PulseOsc(): n(Samples * 2)
+	PulseOsc(): n(Samples * 2), nmask(Samples * 2 - 1)
 	{
 		pw1t = false;
 		bP1 = 0;
@@ -169,13 +169,13 @@ public:
 		for (int i = 0; i < n; i++)
 		{
 			float mixvalue = blepPTR[lpIn] * f1 + blepPTR[lpIn + Blepsize] * frac;
-			buf[(bpos + i) & (n - 1)] += mixvalue * scale;
+			buf[(bpos + i) & nmask] += mixvalue * scale;
 			lpIn++;
 		}
 	}
 	inline float getNextBlep(float *buf, int &bpos)
 	{
-		bpos = (bpos + 1) & (n - 1);
+		bpos = (bpos + 1) & nmask;
 		float value = buf[bpos];
 		buf[bpos] = 0.0f;
 
--- plugins/mimid/Engine/SawOsc.h
+++ plugins/mimid/Engine/SawOsc.h
@@ -28,11 +28,11 @@
 class SawOsc
 {
 	float buffer1[Samples * 2];
-	const int n;
+	const int n, nmask;
 	float const *blepPTR;
 	int bP1;
 public:
-	SawOsc(): n(Samples * 2)
+	SawOsc(): n(Samples * 2), nmask(Samples * 2 - 1)
 	{
 		bP1 = 0;
 		for (int i = 0; i < n; i++)
@@ -106,13 +106,13 @@ public:
 		for (int i = 0 ; i < n; i++)
 		{
 			float mixvalue = blepPTR[lpIn] * f1 + blepPTR[lpIn + Blepsize] * frac;
-			buf[(bpos + i) & (n - 1)] += mixvalue * scale;
+			buf[(bpos + i) & nmask] += mixvalue * scale;
 			lpIn++;
 		}
 	}
 	inline float getNextBlep(float *buf, int &bpos)
 	{
-		bpos = (bpos + 1) & (n - 1);
+		bpos = (bpos + 1) & nmask;
 		float value = buf[bpos];
 		buf[bpos] = 0.0f;
 
