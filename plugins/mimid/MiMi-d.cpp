f4f244849117af1ee40e831ce51cd62bff3c38f7
Ricard Wanderlof <ricard2013@butoba.net>
2026-02-19 06:48:26 +0100

    MiMi-d: Set plugin version to 2.2.0 for release
    The version brings in the new PolyD (Dual) key assign mode which
    triggers two potentially stereo panned voices per note played.
    A third LFO has been added, which has a continuous shape control
    instead of the stepped waveform of LFO1 and 2. Finally, a new
    Controller menu allows direct control of modulation from mod wheel
    and after touch.
    
    A couple of minor bugs have been fixed, one involving the limiting
    function for the shape parameter for the oscillators for the saw
    waveform, and one concerning hanging notes when notes are played
    while switching between poly and mono modes.
    
    A few presets have been added showcasing the new dual mode, and
    a couple of presets have been updated. All presets now have the
    voice count set to 12 instead of 6 to better allow for switching
    to dual mode without loosing too much polyphony.

--- plugins/mimid/MiMi-d.cpp
+++ plugins/mimid/MiMi-d.cpp
@@ -152,7 +152,7 @@ protected:
 	}
 	const char *getMaker() const override { return "Pollux"; }
 	const char *getLicense() const override { return "GPL2"; }
-	uint32_t getVersion() const override { return d_version(2,1,2); }
+	uint32_t getVersion() const override { return d_version(2,2,0); }
 	int64_t getUniqueId() const override {
 	    return d_cconst('M','i','M','d');
 	}
