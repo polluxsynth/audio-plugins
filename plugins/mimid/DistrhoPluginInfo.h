f33464bf242a0f02011ca92c4380dc107a108a66
Ricard Wanderlof <ricard2013@butoba.net>
2026-09-15 18:22:55 +0200

    MiMi-d, DistrhoPluginInfo: Migrate to DISTRHO_PLUGIN_UNIQUE_ID
    Instead of returning the UNIQUE_ID from a method in the MiMid class,
    define DISTRHO_PLUGIN_UNIQUE_ID, and let DPF handle it appropriately.
    
    This is in preparation for adding AU support to the plugin build,
    which requires the DISTRHO_PLUGIN_UNIQUE_ID macro.

--- plugins/mimid/DistrhoPluginInfo.h
+++ plugins/mimid/DistrhoPluginInfo.h
@@ -23,10 +23,11 @@
 #define _DISTRHO_PLUGIN_INFO_H_
 
 #define DISTRHO_PLUGIN_BRAND "Pollux"
-#define DISTRHO_PLUGIN_BRAND_ID Pllx
-#define DISTRHO_PLUGIN_NAME  "MiMi-d"
-#define DISTRHO_PLUGIN_URI   "https://butoba.net/homepage/mimid.html"
-#define DISTRHO_PLUGIN_CLAP_ID "com.polluxsynth.mimid"
+#define DISTRHO_PLUGIN_BRAND_ID   Pllx
+#define DISTRHO_PLUGIN_UNIQUE_ID  MiMd
+#define DISTRHO_PLUGIN_NAME       "MiMi-d"
+#define DISTRHO_PLUGIN_URI        "https://butoba.net/homepage/mimid.html"
+#define DISTRHO_PLUGIN_CLAP_ID    "com.polluxsynth.mimid"
 
 #define PLUGIN_VERSION_MAJOR 2
 #define PLUGIN_VERSION_MINOR 4
