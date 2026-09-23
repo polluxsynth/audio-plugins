31b9c4f13156c96aabbcdefbbbc4331df567b6ae
Ricard Wanderlof <ricard2013@butoba.net>
2026-09-23 23:10:37 +0200

    DistrhoPluginInfo: Set version number to 2.4.3 for relase

--- plugins/mimid/DistrhoPluginInfo.h
+++ plugins/mimid/DistrhoPluginInfo.h
@@ -31,7 +31,7 @@
 
 #define PLUGIN_VERSION_MAJOR 2
 #define PLUGIN_VERSION_MINOR 4
-#define PLUGIN_VERSION_MICRO 2
+#define PLUGIN_VERSION_MICRO 3
 
 #define DISTRHO_PLUGIN_NUM_INPUTS                    0
 #define DISTRHO_PLUGIN_NUM_OUTPUTS                   2
